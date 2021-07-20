a58f8d39b2bc10c5c2ab479ddcd998777be378a9
anupkalita <10anupkalita@gmail.com>
2021-07-21 01:06:59 +0530

    refactor: added dynamic port

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.post("/", function (req, res) {
   request.end();
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server is running on port 3000");
 });
 
